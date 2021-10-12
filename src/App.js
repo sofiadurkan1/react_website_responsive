ef016575e798b80763f99299da4cb19e5fcf2b95
Sofia <sofiadurkan@gmail.com>
2021-10-12 00:50:14 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import{ Navbar } from "./components"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Nav } from './Navbar.elements'
+
+
+
+const Navbar = () => {
+    return (
+       <>
+       <Nav>
+           Test
+       </Nav>
+
+       
+       </>
+
+    )
+}
+
+export default Navbar;
