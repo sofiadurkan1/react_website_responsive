c875a6742eaf9cdd1da3edb022349189065d0e4c
Sofia <sofiadurkan@gmail.com>
2021-10-14 10:30:18 -0500

    info section added

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,3 +1,12 @@
 export const homeObjOne = {
-    lightBg:false
+    lightBg:false,
+    primary:true,
+    topLine:'Dynamic Web Applications',
+    lightTopLine:'true',
+    lightText:'true',
+    lightTextDesc:'true',
+    headline:'Frontend Developer|React Developer',
+    description:'Welcome to web page here is my projects......',
+    buttonLabel:'Get Started',
+    imgStart: '',
 }
